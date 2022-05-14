6b870cb71189411b99fdf66f3c41ad8cb7530c94
Thomas <tmanti17@gmail.com>
2022-05-14 16:08:00 -0400

    save 05/14/2022 4:07

--- DriverTrimFrontEnd/component/NavBar.tsx
+++ DriverTrimFrontEnd/component/NavBar.tsx
@@ -1,18 +1,10 @@
-import { useRouter } from 'next/router';
-import styles from '../styles/Navbar.module.css';
+import React from 'react';
+import { Router, useRouter } from 'next/router';
 
-const Navbar = ({linkCol}:{ linkCol: string }) =>{//https://github.com/maticzav/nookies#readme
+function logoImage() {
     const router = useRouter();
-
-    return (
-        <div className = { styles.navbarBackground }>
-            <div className = { styles.navbarLinkContainer }>
-                <h1 className = { styles.navbarLink } style = {{ color: linkCol }}> 
-                    <img src = '/favicon.ico' style = {{ transform: 'translate(0,-15px)', height: '60px' }} onClick = { () => router.push('/') } />
-                </h1>
-            </div>
-        </div>
-    );
+  // Import result is the URL of your image
+    return <img src={'/favicon.ico'} alt="Logo" onClick={ () => router.push('/')}/>;
 }
 
-export default Navbar;
+export default logoImage;
