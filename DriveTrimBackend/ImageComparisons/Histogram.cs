e1a390ed9c158b8a68c840868ce1d684229ec89b
tmanti <tmanti17@gmail.com>
2022-05-14 15:46:17 -0400

    histogram

--- DriveTrimBackend/ImageComparisons/Histogram.cs
+++ DriveTrimBackend/ImageComparisons/Histogram.cs
@@ -1,10 +1,12 @@
-﻿namespace ImageComparisons
+﻿using System;
+
+namespace ImageComparisons
 {
     public class Histogram
     {
-        public int[] red_hist = new int[5]; //51 each
-        public int[] green_hist = new int[5]; //51 each
-        public int[] blue_hist = new int[5]; //51 each
+        private int[] _redHist; //52 each
+        private int[] _greenHist; //52 each
+        private int[] _blueHist; //52 each
 
         //public int[] texture_direction_hist;
         //public int[] texture_scale_hist;
@@ -12,7 +14,29 @@
         
         public Histogram()
         {
-            
+            _redHist = new [] {0, 0, 0, 0, 0};
+            _greenHist = new [] {0, 0, 0, 0, 0};
+            _blueHist = new[] {0, 0, 0, 0, 0};
+        }
+
+        public void Count_Red(int val)
+        {
+            _redHist[val / 52]++;
+        }
+
+        public void Count_Green(int val)
+        {
+            _greenHist[val / 52]++;
+        }
+
+        public void Count_Blue(int val)
+        {
+            _blueHist[val / 52]++;
+        }
+
+        public void Print_Hist()
+        {
+            Console.WriteLine("Red: " + String.Join(",", _redHist) + " | Green: " + String.Join(",", _greenHist) + " | Blue: " + String.Join(",", _blueHist));
         }
     }
 }
