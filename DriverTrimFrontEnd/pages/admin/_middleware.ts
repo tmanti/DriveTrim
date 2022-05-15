9d09af267d9618cf0b52bc2a38d862e8006588b1
tmanti <tmanti17@gmail.com>
2022-05-15 14:04:59 -0400

    middleware fix

--- DriverTrimFrontEnd/pages/admin/_middleware.ts
+++ DriverTrimFrontEnd/pages/admin/_middleware.ts
@@ -3,6 +3,6 @@ import { withAuth } from "next-auth/middleware"
 // More on how NextAuth.js middleware works: https://next-auth.js.org/configuration/nextjs#middleware
 export default withAuth({
   callbacks: {
-    authorized: ({  }) => ?.userRole === "admin",
+    authorized: ({ token }) => token?.userRole === "admin",
   },
 })
